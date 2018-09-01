834406de54f5f2e901265b6ecc2b86cca4c7806d
aliciapflaumer <alpfla7@gmail.com>
2018-09-01 17:44:45 -0400

    2D Lists & Nested Loops

--- app.py
+++ app.py
@@ -280,3 +280,16 @@
 #     return result
 #
 # print(raise_to_power(2, 3))
+
+
+# 2D Lists & Nested Loops
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
